c1fe3b2e20f38402dc0258e302d9a998d02b6702
mlazzell17@gmail.com <MicheSi>
2019-12-19 13:35:02 -0700

    added base of form jsx

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,1 +1,52 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+
+const NewForm = ({values}) => {
+    return (
+        <div className='form-container'>
+            <Form>
+                <label htmlFor='name'>Name: </label>
+                <Field
+                id='name'
+                type='text'
+                name='name'/>
+                <label htmlFor='email'>Email: </label>
+                <Field
+                id='email'
+                type='text'
+                name='email'/>
+                <label htmlFor='password'>Password: </label>
+                <Field
+                id='password'
+                type='text'
+                name='password'/>
+                <label
+                 className='checkbox'
+                 htmlFor='terms'>
+                 Terms of Service
+                 <Field
+                  id='terms'
+                  type='checkbox'
+                  name='terms'
+                  checked={values.terms}
+                 />
+                </label>
+                <button type='submit'>Submit</button>
+                
+
+            </Form>
+            
+        </div>
+
+    )
+}
+
+const FormikForm = withFormik({
+    mapPropsToValues({name}) {
+        return {
+            name: ''
+        }
+    }
+})(NewForm)
+
+export default FormikForm;

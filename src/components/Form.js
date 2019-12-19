9356d67916cb7fa6c5804983d39e15d2c9c79c51
mlazzell17@gmail.com <MicheSi>
2019-12-19 13:55:58 -0700

    added validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const NewForm = ({values}) => {
+const NewForm = ({values, errors, touched}) => {
     return (
         <div className='form-container'>
             <Form>
@@ -10,16 +11,25 @@ const NewForm = ({values}) => {
                 id='name'
                 type='text'
                 name='name'/>
+                {touched.name && errors.name && (
+                    <p className='errors'>{errors.name}</p>
+                )}
                 <label htmlFor='email'>Email: </label>
                 <Field
                 id='email'
                 type='text'
                 name='email'/>
+                {touched.email && errors.email && (
+                    <p className='errors'>{errors.email}</p>
+                )}
                 <label htmlFor='password'>Password: </label>
                 <Field
                 id='password'
                 type='text'
                 name='password'/>
+                {touched.password && errors.password && (
+                    <p className='errors'>{errors.password}</p>
+                )}
                 <label
                  className='checkbox'
                  htmlFor='terms'>
@@ -30,6 +40,9 @@ const NewForm = ({values}) => {
                   name='terms'
                   checked={values.terms}
                  />
+                 {touched.terms && errors.terms && (
+                    <p className='errors'>{errors.terms}</p>
+                )}
                 </label>
                 <button type='submit'>Submit</button>
                 
@@ -42,11 +55,19 @@ const NewForm = ({values}) => {
 }
 
 const FormikForm = withFormik({
-    mapPropsToValues({name}) {
+    mapPropsToValues({name, email, password}) {
         return {
-            name: ''
+            name: '',
+            email: '',
+            password: ''
         }
-    }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().required(),
+        password: Yup.string().required(),
+        terms: Yup.boolean().oneOf([true], "Must accept Terms of Service")
+    })
 })(NewForm)
 
 export default FormikForm;
